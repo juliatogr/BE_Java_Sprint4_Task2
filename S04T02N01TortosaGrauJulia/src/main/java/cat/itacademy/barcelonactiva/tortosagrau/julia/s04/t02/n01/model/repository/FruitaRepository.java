848920bf4c871ebcaba4fa856541bc4bbabf97d6
juliatogr <julia.tortosa01@estudiant.upf.edu>
2022-11-16 10:24:22 +0100

    N1 more cleaned

--- S04T02N01TortosaGrauJulia/src/main/java/cat/itacademy/barcelonactiva/tortosagrau/julia/s04/t02/n01/model/repository/FruitaRepository.java
+++ S04T02N01TortosaGrauJulia/src/main/java/cat/itacademy/barcelonactiva/tortosagrau/julia/s04/t02/n01/model/repository/FruitaRepository.java
@@ -1,7 +1,5 @@
 package cat.itacademy.barcelonactiva.tortosagrau.julia.s04.t02.n01.model.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
